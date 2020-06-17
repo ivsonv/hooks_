a6df9f64735fb7dffdd127f771fac71f7aceff71
Ivson Vital <ivson@vorso.com.br>
2020-06-17 08:22:39 -0300

    use memo- react-hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [tech, setTech] = useState([]);
@@ -25,6 +25,8 @@ function App() {
     localStorage.setItem('tech-list', JSON.stringify(tech))
   }, [tech]);
 
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
       <ul>
@@ -34,6 +36,7 @@ function App() {
           ))
         }
       </ul>
+      <strong>tecs {techSize}</strong>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={add}>add</button>
     </>
