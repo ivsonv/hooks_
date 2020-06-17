6de2249481a0f2e2300acd541cb42ce43e86f416
Ivson Vital <ivson@vorso.com.br>
2020-06-17 08:09:34 -0300

    usando o useEffect do hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['react', 'react-js']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function add() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storetech = localStorage.getItem('tech-list');
+
+    if (storetech) {
+      setTech(JSON.parse(storetech));
+    }
+
+    // return () => {
+    //   document.removeEventListener
+    //  };
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech-list', JSON.stringify(tech))
+  }, [tech]);
 
   return (
     <>
